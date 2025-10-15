009f30f6cad23d9cfe90680b9e6a61fd7155b952
Bidhan Ranjan Bhoi <124060161+777BRB@users.noreply.github.com>
2025-10-15 15:45:19 +0530

    Create 0-1 Knapsack Problem.java

---
+++ 0-1 Knapsack Problem.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int knapsack(int[] weights, int[] values, int W) {
+        int n = weights.length;
+        int[][] dp = new int[n+1][W+1];
+        for(int i=1;i<=n;i++){
+            for(int w=0;w<=W;w++){
+                if(weights[i-1]<=w)
+                    dp[i][w] = Math.max(dp[i-1][w], values[i-1]+dp[i-1][w-weights[i-1]]);
+                else
+                    dp[i][w] = dp[i-1][w];
+            }
+        }
+        return dp[n][W];
+    }
+}
